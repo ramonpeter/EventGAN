23ecc5d933919016c9fe266dd4c51af08b123003
Ramon Winterhalder <winterhalder@thphys.uni-heidelberg.de>
2021-04-25 20:46:32 +0200

    Fix kernels

--- modules/kernels.py
+++ modules/kernels.py
@@ -2,6 +2,7 @@
 
 import tensorflow as tf
 
+
 # pylint: disable=invalid-name
 def squared_pairwise_dist(X, Y):
     """Computes the squared pairwise Euclidean distances between x and y.
@@ -18,9 +19,9 @@ def squared_pairwise_dist(X, Y):
     """
 
     if not len(X.get_shape()) == len(Y.get_shape()) == 2:
-        raise ValueError('Both inputs should be matrices.')
+        raise ValueError("Both inputs should be matrices.")
     if X.get_shape().as_list()[1] != Y.get_shape().as_list()[1]:
-        raise ValueError('The number of features should be the same.')
+        raise ValueError("The number of features should be the same.")
 
     XX = tf.matmul(X, X, transpose_b=True)
     XY = tf.matmul(X, Y, transpose_b=True)
@@ -29,10 +30,14 @@ def squared_pairwise_dist(X, Y):
     X_sqnorms = tf.linalg.diag_part(XX)
     Y_sqnorms = tf.linalg.diag_part(YY)
 
-    r = lambda x: tf.expand_dims(x, 0)
-    c = lambda x: tf.expand_dims(x, 1)
+    def r(x):
+        return tf.expand_dims(x, 0)
+
+    def c(x):
+        return tf.expand_dims(x, 1)
 
     return c(X_sqnorms) + r(Y_sqnorms) - 2 * XY
+
 
 def mix_gaussian_kernel(X, Y, sigmas, wts=None, K_XY_only=False):
     """Computes a Gaussian Radial Basis Kernel between the samples of X and Y.
@@ -50,26 +55,30 @@ def mix_gaussian_kernel(X, Y, sigmas, wts=None, K_XY_only=False):
         wts = [1] * len(sigmas)
     else:
         if not len(wts) == len(sigmas):
-            raise ValueError('The number of weights need to be equal to number of sigmas.')
+            raise ValueError(
+                "The number of weights need to be equal to number of sigmas."
+            )
 
     K_XX, K_XY, K_YY = 0.0, 0.0, 0.0
 
     for sigma, wt in zip(sigmas, wts):
-        gamma = 1.0 / (2. * sigma**2)
+        gamma = 1.0 / (2.0 * sigma ** 2)
         K_XY += wt * tf.exp(-gamma * squared_pairwise_dist(X, Y))
 
     if K_XY_only:
         return K_XY
 
     for sigma, wt in zip(sigmas, wts):
-        gamma = 1.0 / (2. * sigma**2)
+        gamma = 1.0 / (2.0 * sigma ** 2)
         K_XX += wt * tf.exp(-gamma * squared_pairwise_dist(X, X))
         K_YY += wt * tf.exp(-gamma * squared_pairwise_dist(Y, Y))
 
     return K_XX, K_XY, K_YY
 
+
 # Cauchy distribution
 #
+
 
 def mix_cauchy_kernel(X, Y, alphas, wts=None, K_XY_only=False):
     """Computes a mixture of cauchy kernels between
@@ -85,26 +94,30 @@ def mix_cauchy_kernel(X, Y, alphas, wts=None, K_XY_only=False):
         wts = [1] * len(alphas)
     else:
         if not len(wts) == len(alphas):
-            raise ValueError('The number of weights need to be equal to number of sigmas.')
+            raise ValueError(
+                "The number of weights need to be equal to number of sigmas."
+            )
 
     K_XX, K_XY, K_YY = 0, 0, 0
 
     for alpha, wt in zip(alphas, wts):
-        gamma = 1 / (alpha**2)
+        gamma = 1 / (alpha ** 2)
         K_XY += wt * 1 / (1 + gamma * squared_pairwise_dist(X, Y))
 
     if K_XY_only:
         return K_XY
 
     for alpha, wt in zip(alphas, wts):
-        gamma = 1 / (alpha**2)
+        gamma = 1 / (alpha ** 2)
         K_XX += wt * 1 / (1 + gamma * squared_pairwise_dist(X, X))
         K_YY += wt * 1 / (1 + gamma * squared_pairwise_dist(Y, Y))
 
     return K_XX, K_XY, K_YY
 
+
 # Breit-Wigner distribution
 #
+
 
 def mix_breit_wigner_kernel(X, Y, alphas, wts=None, K_XY_only=False):
     """Computes a mixture of Breit-Wigner kernels between
@@ -120,19 +133,21 @@ def mix_breit_wigner_kernel(X, Y, alphas, wts=None, K_XY_only=False):
         wts = [1] * len(alphas)
     else:
         if not len(wts) == len(alphas):
-            raise ValueError('The number of weights need to be equal to number of sigmas.')
+            raise ValueError(
+                "The number of weights need to be equal to number of sigmas."
+            )
 
     K_XX, K_XY, K_YY = 0, 0, 0
 
     for alpha, wt in zip(alphas, wts):
-        gamma = 4 / (alpha**2)
+        gamma = 4 / (alpha ** 2)
         K_XY += wt * 1 / (1 + gamma * squared_pairwise_dist(X, Y))
 
     if K_XY_only:
         return K_XY
 
     for alpha, wt in zip(alphas, wts):
-        gamma = 4 / (alpha**2)
+        gamma = 4 / (alpha ** 2)
         K_XX += wt * 1 / (1 + gamma * squared_pairwise_dist(X, X))
         K_YY += wt * 1 / (1 + gamma * squared_pairwise_dist(Y, Y))
 
